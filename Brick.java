8544adb20f9d6ef9dbfe6921a3a42fc717854909
Rehan Kazi <67158116+RehanKazi@users.noreply.github.com>
2022-11-11 09:58:42 -0500

    Add files via upload

--- Brick.java
+++ Brick.java
@@ -1,13 +1,11 @@
 public class Brick {
-    // Dimensions
-                           //  _____            _               
-    public double width;   // |_   _|          | |              
-    public double height;  //   | |  _ __   ___| |__   ___  ___ 
-    public double length;  //   | | | '_ \ / __| '_ \ / _ \/ __|
-                           //  _| |_| | | | (__| | | |  __/\__ \
-                           // |_____|_| |_|\___|_| |_|\___||___/
+   
+    // Dimensions                                                        
+    private double width;    //inches
+    private double height; //inches
+    private double length;//inches
 
-
+   //Properties
     private String material;
     private double weight; //weight in lbs
     private String color;
@@ -19,9 +17,91 @@ public class Brick {
         length = 8.46;
         width = 4.4;
         height = 2.5;
+        weight = 4.3;
+        material = "clay";
+        color = "red";
+        sturdiness = 1700;
+        lifeTime = 650;
     }
     
-    public Brick (int length, int width, int height, int weight, int lifeTime) {
+    public String toString() {
+        return "L: " +  length + ", W; " + width + "  H: " + height + "/n" + "Color: " + color + "/n" + "Weight: " + weight;
+    }
+    
+    public double getWidth () {
+        return width;
+    }
 
+    public double getHeight() {
+        return height;
+    }
+
+    public double getLength() {
+        return height;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getSturdiness() {
+        return sturdiness;
+    }
+
+    public int returnLifeTime() {
+        return lifeTime;
+    }
+
+    public void setWidth(double w) {
+        width = w;
+    }
+
+    public void setHeight(double h) {
+        height = h;
+    }
+
+    public void setLength(double l) {
+        length = l;
+    }
+
+    public void setMaterial(String m) {
+        material = m;
+    }
+
+    public void setWeight(double w) {
+        weight = w;
+    }
+
+    public void setColor(String c) {
+        color = c;
+    }
+
+    public void setSturdiness(double s) {
+        sturdiness = s;
+    }
+
+    public void setLifeTime(int y) {
+        lifeTime = y;
+    }
+
+    public double calculateVolume() {
+        return length * width * height;
+    }
+
+    public double getDensity() {
+        return weight / this.calculateVolume();
+    }
+
+    public void damage() {
+        sturdiness *= 0.8;
+        lifeTime *= 0.8;
     }
 }
